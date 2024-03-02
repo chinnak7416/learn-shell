03b6433186b161c180374470c8d12d65eae09211
RAMBABU <chinnak7416@gmail.com>
2024-03-02 21:41:30 +0530

    varaibles

---
+++ 02-vars.sh
@@ -0,0 +1,9 @@
+#varaible with plane data
+a=10
+
+#varaible with special character data(space is also special char )
+b="Hello World"
+
+
+echo $a   #for printing a var
+echo $b   #for printing b var
