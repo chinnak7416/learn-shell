bff4f26991396adefc940e79f53836336d022476
RAMBABU <chinnak7416@gmail.com>
2024-03-11 22:58:10 +0530

    substitutions demo

--- 02-vars.sh
+++ 02-vars.sh
@@ -7,3 +7,18 @@ b="Hello World"
 
 echo $a   #for printing a var
 echo $b   #for printing b var
+#substitutions
+#command sub
+# A command output if we want in a variable then it is command sub
+#syntax: var=$(command)
+x=$(uptime)
+echo $x
+
+#Arithmetic sub
+# A arithmetic expression is solved and output to a variable then it is Arithmetic sub
+y=$((3+4))
+echo $y
+z=$((3-5*9+8/2))
+echo $z
+
+
